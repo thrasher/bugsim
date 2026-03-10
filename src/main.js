beb36654e4f26d50765348e0811710472b0c7bcc
thrasher <jasonthrasher@gmail.com>
2026-03-10 19:57:38 +0100

    refactored how the timestamp is loaded

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import './style.css'
+import { initTimestamp } from './timestamp.js';
 
-document.querySelector('#app').innerHTML = ``
+document.querySelector('#app').innerHTML = `
+    <div id="datetime-overlay" style="position: fixed; top: 10px; left: 10px; background: black; color: white; padding: 5px 10px; font-family: monospace; z-index: 10000000; border-radius: 4px;"></div>
+`
+
+initTimestamp('datetime-overlay');
